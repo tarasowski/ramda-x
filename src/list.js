0eed8204cd5c7a74d8de38e6a551214fef9d656d
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2018-12-09 17:11:01 +0100

    Create list.js

---
+++ src/list.js
@@ -0,0 +1,10 @@
+const List = x => ({
+    empty: List(),
+    map: f => List(x.map(f)),
+    filter: f => List(x.filter(f)),
+    reduce: f => List(x.reduce(f)),
+    fold: f => f(x),
+    inspect: () => `List(${x})`
+})
+
+module.exports = List
