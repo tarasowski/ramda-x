253cfbdfcc7e74ea9ef537e8d7a606f018fafae9
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2018-12-11 07:32:21 +0100

    Update task.js

--- src/task.js
+++ src/task.js
@@ -2,18 +2,18 @@ const compose = (...fns) => x =>
     fns.reduceRight((v, f) => f(v), x)
 
 
-const Task = computation => ({
-    fork: computation,
-    map(fn) {
-        return Task((reject, resolve) => this.fork(reject, compose(resolve, fn)));
-    },
-    chain(fn) {
-        return Task((reject, resolve) => this.fork(reject, x => fn(x).fork(reject, resolve)));
-    },
-    ap(f) {
-        return this.chain(fn => f.map(fn))
-    }
-})
+const Task = computation => {
+    const fork = computation
+    const map = fn =>
+        Task((reject, resolve) =>
+            fork(reject, compose(resolve, fn)))
+    const chain = fn =>
+        Task((reject, resolve) =>
+            fork(reject, x => fn(x).fork(reject, resolve)))
+    const ap = f =>
+        chain(fn => f.map(fn))
+    return { fork, map, chain, ap, }
+}
 
 Task.of = x => Task((_, resolve) => resolve(x))
 Task.rejected = a => Task((reject, _) => reject(a))
