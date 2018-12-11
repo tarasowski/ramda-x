bf02bafa7358273b045c2313cf89602225a72c4e
Dimitri Tarasowski <33748291+tarasowski@users.noreply.github.com>
2018-12-11 06:28:57 +0100

    Update list.js

--- src/list.js
+++ src/list.js
@@ -3,13 +3,11 @@ const List = xs => ({
     map: fn => List(xs.map(fn)),
     reduce: (f, i) => List(xs.reduce(f, i)),
     fold: f => f(xs),
-    traverse(of, fn) {
-        return xs.reduce(
+    traverse: (of, fn) =>
+        xs.reduce(
             (f, a) => fn(a).map(b => bs => bs.concat(b)).ap(f),
             of(List([]))
         )
-    }
 })
-
 
 module.exports = List
